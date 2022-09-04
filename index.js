4fb46c81181ca75fae5f4d5b393c16d5dd3ea8f0
Michaela Beakley <bullfrogbst@gmail.com>
2022-09-04 16:17:45 -0500

    Create basic JavaScript pseudocode

---
+++ index.js
@@ -0,0 +1,19 @@
+// Access all necessary elements
+
+// Detect when the event form is submitted, collect the data, and call the makeEvent() function with the data passed in.
+
+// Declare makeEvent()
+
+// Make a new card element with the data stored in the correct places if there's less than 12 events total
+
+// Every second, call the countDown() function
+
+// Declare countDown()
+
+// For every event, calculate how much time is left until the event happens, and set the value of the timer to that.
+
+// When a delete button is clicked, find the parent event, and pass it in to the deleteCard() function
+
+// Declare deleteCard()
+
+//
