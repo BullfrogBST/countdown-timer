984694fb0b2c9ba1cab953b9ad0f5761bbd96fc6
Michaela Beakley <bullfrogbst@gmail.com>
2022-09-04 17:00:23 -0500

    Update pseudocode and get form data when submitted

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 // Access all necessary elements
-const eventForm = document.querySelector(".event-form");
-const eventGrid = document.querySelector(".event-grid");
+const eventForm = document.querySelector('.event-form');
+let eventData = new FormData(eventForm);
+
+const eventNameInput = document.querySelector('.event-name-input');
+const eventDateInput = document.querySelector('.event-date-input');
+const eventTimeInput = document.querySelector('.event-time-input');
+
+const eventGrid = document.querySelector('.event-grid');
 
 // Detect when the event form is submitted, collect the data, and call the makeEvent() function with the data passed in.
+eventForm.addEventListener('submit', e => {
+    eventData.append('eventName', eventNameInput.value);
+    eventData.append('eventDate', eventDateInput.value);
+    eventData.append('eventTime', eventTimeInput.value);
 
+    makeEvent(eventData.get('eventName'), eventData.get('eventDate'), eventData.get('eventTime'))
+});
 // Declare makeEvent()
-
+function makeEvent(eventName, eventDate, eventTime) {
+    console.log(eventName);
+    console.log(eventDate);
+    console.log(eventTime);
+}
 // Check if there are less than 12 cards
 
 // Make a new card element with a unique ID and the data stored in the correct places
