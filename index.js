87146686e748a8e1a550e79b539803583adad597
Michaela Beakley <bullfrogbst@gmail.com>
2022-09-04 17:12:33 -0500

    push new instance of the Event class when a form is submitted, and update pseudocode.

--- index.js
+++ index.js
@@ -12,35 +12,41 @@ class Event {
         this.time = null;
     }
 }
+
 // Access all necessary elements
-const eventForm = document.querySelector('.event-form');
+
+const eventForm = document.querySelector ('.event-form');
 let eventData = new FormData(eventForm);
 
-const eventNameInput = document.querySelector('.event-name-input');
-const eventDateInput = document.querySelector('.event-date-input');
-const eventTimeInput = document.querySelector('.event-time-input');
+const eventNameInput = document.querySelector ('.event-name-input');
+const eventDateInput = document.querySelector ('.event-date-input');
+const eventTimeInput = document.querySelector ('.event-time-input');
 
 const eventGrid = document.querySelector('.event-grid');
 
-// Detect when the event form is submitted, collect the data, and call the makeEvent() function with the data passed in.
+const allEvents = [];
+
+// Detect when the event form is submitted, collect the data, and call the makeEvent() function with the data passed in
+
 eventForm.addEventListener('submit', e => {
     eventData.append('eventName', eventNameInput.value);
     eventData.append('eventDate', eventDateInput.value);
     eventData.append('eventTime', eventTimeInput.value);
 
-    makeEvent(eventData.get('eventName'), eventData.get('eventDate'), eventData.get('eventTime'))
+    makeEvent ( eventData.get('eventName'), eventData.get('eventDate'), eventData.get('eventTime') )
 });
-// Declare makeEvent()
-function makeEvent(eventName, eventDate, eventTime) {
-    console.log(eventName);
-    console.log(eventDate);
-    console.log(eventTime);
+
+// Declare makeEvent ()
+
+function makeEvent (eventName, eventDate, eventTime) {
+    // Check if there are less than 12 cards
+
+    if ( allEvents.length < 12 ){
+        // Make a new card element with the data stored in the correct places
+
+        allEvents.push ( new Event(eventName, eventDate, eventTime) );
+    }
 }
-// Check if there are less than 12 cards
-
-// Make a new card element with a unique ID and the data stored in the correct places
-
-// Set the eventCards variable to be all of the event cards
 
 // Every second, call the countDown() function
 
